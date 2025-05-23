34b724ed2593171bc99f9622c0c1ff1698711345
Mikio Takeuchi <mtake@jp.ibm.com>
2025-05-23 06:01:31 -0500

    script for vllm

---
+++ examples/data-generation-rits/vllm_phi4reasoningplus.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+# for macOS
+if command -v gdate &> /dev/null
+then
+    DATE_CMD=gdate
+else
+    DATE_CMD=date
+fi
+
+START_TIME="$(${DATE_CMD} +%s)"
+START_TIME_STR="$(${DATE_CMD} -d @${START_TIME} +%Y%m%d-%H%M%S)"
+BASENAME="$(basename "${BASH_SOURCE}" .sh)"
+HOSTNAME_S="$(hostname -s)"
+LOGFILE="${BASENAME}-${START_TIME_STR}-${HOSTNAME_S}.log"
+echo "XXX LOGFILE ${LOGFILE}" | tee -a ${LOGFILE}
+echo "XXX DATETIME ${START_TIME_STR}" | tee -a ${LOGFILE}
+
+OPTS=""
+if [[ "$(hostname -s)" = "graphbm" ]]; then
+    OPTS="${OPTS} --dtype float16"
+    #OPTS="${OPTS} --max-model-len 16384"
+    OPTS="${OPTS} --max-model-len 2048"
+    OPTS="${OPTS} --gpu-memory-utilization 0.95"
+fi
+
+ENV=""
+#ENV="TOKENIZERS_PARALLELISM=false ${ENV}"
+#ENV="PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True ${ENV}"
+
+cmd="${ENV}vllm serve microsoft/Phi-4-reasoning-plus --enable-reasoning --reasoning-parser deepseek_r1${OPTS}"
+echo "$cmd" | tee -a ${LOGFILE}
+eval "$cmd" 2>&1 | tee -a ${LOGFILE}
+
+END_TIME="$(${DATE_CMD} +%s)"
+END_TIME_STR="$(${DATE_CMD} -d @${END_TIME} +%Y%m%d-%H%M%S)"
+echo "XXX DATETIME ${END_TIME_STR}" | tee -a ${LOGFILE}
+echo "XXX ELAPSED_SECS $((END_TIME - START_TIME))" | tee -a ${LOGFILE}
