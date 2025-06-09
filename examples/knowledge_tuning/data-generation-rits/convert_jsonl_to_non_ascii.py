c75fa03285e196047101a786cfb272fdb922a4e4
Mikio Takeuchi <mtake@jp.ibm.com>
2025-06-09 04:13:52 -0400

    rename teigaku-genzei-ibm datasets

--- examples/knowledge_tuning/data-generation-rits/convert_jsonl_to_non_ascii.py
+++ examples/knowledge_tuning/data-generation-rits/convert_jsonl_to_non_ascii.py
@@ -2,7 +2,9 @@ import pandas as pd
 
 # data_prefix = "seed_data_"
 data_prefix = "messages_data_"
-data_name = "teigaku-genzei"
+# data_name = "teigaku-genzei"
+# data_name = "teigaku-genzei-ibm-v0"
+data_name = "teigaku-genzei-ibm-v2"
 data_path = f"{data_prefix}{data_name}.jsonl"
 data_path_non_ascii = f"{data_prefix}{data_name}_non_ascii.jsonl"
 
