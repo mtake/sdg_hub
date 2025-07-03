3fb9109120ceff4809f0877beedc1b3844da35b4
Mikio Takeuchi <mtake@jp.ibm.com>
2025-07-04 01:40:40 +0900

    update script

--- examples/knowledge_tuning/data-generation-rits/convert_jsonl_to_non_ascii.py
+++ examples/knowledge_tuning/data-generation-rits/convert_jsonl_to_non_ascii.py
@@ -1,10 +1,12 @@
 import pandas as pd
 
 # data_prefix = "seed_data_"
-data_prefix = "messages_data_"
+# data_prefix = "messages_data_"
 # data_name = "teigaku-genzei"
 # data_name = "teigaku-genzei-ibm-v0"
-data_name = "teigaku-genzei-ibm-v2"
+# data_name = "teigaku-genzei-ibm-v2"
+# data_name = "ibm-newsroom"
+data_name = "ibm-newsroom-d5"
 data_path = f"{data_prefix}{data_name}.jsonl"
 data_path_non_ascii = f"{data_prefix}{data_name}_non_ascii.jsonl"
 
