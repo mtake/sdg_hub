26f42ce1d2308d8274489ea908aaf317fdbe9024
Mikio Takeuchi <mtake@jp.ibm.com>
2025-09-02 15:23:42 +0900

    WIP adjust parallelism

--- examples/knowledge_tuning/instructlab-mtake/knowledge_generation_and_mixing-mtake.py
+++ examples/knowledge_tuning/instructlab-mtake/knowledge_generation_and_mixing-mtake.py
@@ -317,9 +317,9 @@ print(f"Loaded {len(ds)} seed data", flush=True)
 # max_concurrency = None
 checkpoint_dir = f"{output_dir}_ckpt"
 # save_freq = 1000  # Request timed out. - timeout value=600.0, time taken=1802.91 seconds
-save_freq = 100  # Runs at 30 minutes per chunk. Fails often.
-# save_freq = 10  # Creates 2000+ checkpoints
-max_concurrency = 30
+# save_freq = 100  # Runs at 30 minutes per chunk. Fails often.
+save_freq = 10  # Creates 2000+ checkpoints
+max_concurrency = 20
 # max_concurrency = 10
 
 # %%
