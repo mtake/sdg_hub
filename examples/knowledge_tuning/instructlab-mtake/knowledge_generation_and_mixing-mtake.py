c0205a74eac637414a6858c4f60fd95d71f5eec8
Mikio Takeuchi <mtake@jp.ibm.com>
2025-09-09 18:16:22 +0900

    WIP async_mode=True, save_freq=None, max_concurrency=40

--- examples/knowledge_tuning/instructlab-mtake/knowledge_generation_and_mixing-mtake.py
+++ examples/knowledge_tuning/instructlab-mtake/knowledge_generation_and_mixing-mtake.py
@@ -372,17 +372,17 @@ checkpoint_dir = f"{output_dir}_ckpt"
 # save_freq = 100
 # max_concurrency = 30
 
-# [v0.2 best as of 2025-09-08 night] MEM=100G, Finished in 6.9 hours, 18656 QA pairs. samples_processed: 20668, checkpoint_counter: 1
+# [v0.2 20250907-034856] MEM=100G, Finished in 6.9 hours (24679 secs), 18656 QA pairs. samples_processed: 20668, checkpoint_counter: 1
 # save_freq = None
 # max_concurrency = 30
 
-# [v0.2 best as of 2025-09-09 11am] MEM=100G, Finished in 4.5 hours (16275 secs), 18336 QA pairs. samples_processed: 20306, checkpoint_counter: 1
-# save_freq = None
-# max_concurrency = 40
-
-# WIP MEM=100G
+# [v0.2 best 20250908-130424] MEM=100G, Finished in 4.5 hours (16275 secs), 18336 QA pairs. samples_processed: 20306, checkpoint_counter: 1
 save_freq = None
-max_concurrency = 50
+max_concurrency = 40
+
+# [v0.2 20250909-015619] MEM=100G, Unclosed client session. client_session: <aiohttp.client.ClientSession object at 0x14dc52560b60>
+# save_freq = None
+# max_concurrency = 50
 
 # MEM=100G, async_mode: false. Very slow. Killed
 # -n 64, MEM=200G, async_mode: false. Very slow. Killed
